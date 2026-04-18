265da4055c30a4069ef96a58019864508ccbafcc
Heman Saini <heman60490@gmail.com>
2026-04-18 13:22:23 +0530

    added attendance feature and using foreignkey

--- sms/student/models.py
+++ sms/student/models.py
@@ -7,3 +7,22 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = models.CharField(max_length=13)
+    
+class Attendance(models.Model):
+
+    SUBJECT_CHOICES = (
+        ('Math', 'Math'),
+        ('Science', 'Science'),
+        ('History', 'History'),
+        ('English', 'English'),
+    )
+
+    STATUS_CHOICES = (
+        ('Present', 'Present'),
+        ('Absent', 'Absent'),
+    )
+
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, null=True)
+    subject = models.CharField(max_length=15, choices=SUBJECT_CHOICES)
+    date = models.DateField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)    
--- sms/student/views.py
+++ sms/student/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Student
+from .models import Student, Attendance
 
 # Create your views here.
 
 def student_home(request):
 
     students_data = Student.objects.all()
+    attendance = Attendance.objects.all()
 
+     # student_data = Student.objects.filter(....)
+   
     data = {
-        "students_data": students_data
+        "students_data": students_data,
+        "attendance": attendance
     }
 
     return render(request, "student/student_home.html", data)
@@ -21,12 +25,41 @@ def add_student(request):
         student_name = request.POST.get("input_name")
         student_email = request.POST.get("input_email")
         student_phone_number = request.POST.get("input_phone_number")
-
+         
         Student.objects.create(
             name=student_name,
             email=student_email,
             phone_number=student_phone_number
         )
 
-
+        return redirect("student_home")
+    
     return render(request, "student/add_student.html")
+
+def delete_student(request, student_id):
+
+    my_student = Student.objects.get(id=student_id)
+
+    my_student.delete()
+
+    return redirect("student_home")
+
+def update_student(request, student_id):
+
+    student = Student.objects.get(id=student_id)
+
+    if request.method == "POST":
+        student.name = request.POST.get("name")
+        student.email = request.POST.get("email")
+        student.phone_number = request.POST.get("phone_number")
+        
+
+        student.save()
+        
+        return redirect("student_home")
+
+    parameters = {
+        "student": student
+    }
+
+    return render(request, "student/update_student.html", parameters)
